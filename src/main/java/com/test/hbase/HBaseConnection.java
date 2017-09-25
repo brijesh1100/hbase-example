a23c24b3906d1d837ffb1bc55f0cef50af9a1f38
Brijesh Singh <brijesh.caet@gmail.com>
2017-09-25 11:35:28 +0530

    Hbase example

--- src/main/java/com/test/hbase/HBaseConnection.java
+++ src/main/java/com/test/hbase/HBaseConnection.java
@@ -26,19 +26,21 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 public class HBaseConnection {
+	public static String TABLE_NAME = "TESTLOGS";
+	public static String COLUMN_FAMILY = "ANALYTICS";
 
 	public static void main(String[] args) throws IOException {
 		Configuration conf = HBaseConfiguration.create();
 		// zookeeper-
 		// ip-172-17-0-107.ap-southeast-1.compute.internal,ip-172-17-0-112.ap-southeast-1.compute.internal
 		conf.set("hbase.master",
-				"ip-172-17-0-107.ap-southeast-1.compute.internal");
+				"ip-XXX-XX-X-XXX.ap-southeast-1.compute.internal");
 		conf.set("hbase.rootdir",
-				"hdfs://ip-172-17-0-107.ap-southeast-1.compute.internal:8020/apps/hbase/data");
+				"hdfs://ip-XXX-XX-X-XXX.ap-southeast-1.compute.internal:8020/apps/hbase/data");
 		conf.set("hbase.zookeeper.property.clientPort", "2181");
 		conf.set(
 				"hbase.zookeeper.quorum",
-				"ip-172-17-0-107.ap-southeast-1.compute.internal,ip-172-17-0-112.ap-southeast-1.compute.internal");
+				"ip-XXX-XX-X-XXX.ap-southeast-1.compute.internal,ip-XXX-XX-X-XXX.ap-southeast-1.compute.internal");
 		conf.set("zookeeper.znode.parent", "/hbase-unsecure");
 		Connection connection = null;
 		Table table = null;
@@ -46,18 +48,18 @@ public class HBaseConnection {
 			connection = ConnectionFactory.createConnection(conf);
 			Admin admin = connection.getAdmin();
 			HTableDescriptor descriptor = new HTableDescriptor(
-					TableName.valueOf("CVSLOGS"));
-			descriptor.addFamily(new HColumnDescriptor("RXCONNECT"));
+					TableName.valueOf(HBaseConnection.TABLE_NAME));
+			descriptor.addFamily(new HColumnDescriptor(HBaseConnection.COLUMN_FAMILY));
 			if (!admin.isTableAvailable(descriptor.getTableName())) {
 				System.out.println("Creating Table");
 				admin.createTable(descriptor);
-				System.out.println(" Done.");
+				System.out.println("Table creation has been Done.");
 			}
 
 			try {
 				BufferedReader reader = new BufferedReader(
 						new FileReader(
-								"/home/ive/cvsXmlLog.json"));
+								"/home/brijesh/testLog.json"));
 				String line = null;
 				while ((line = reader.readLine()) != null) {
 					line = fixJSON(line);
@@ -66,12 +68,12 @@ public class HBaseConnection {
 							.fromJson(line, JsonElement.class);
 					JsonObject object = element.getAsJsonObject();
 					Iterator<String> it = object.keySet().iterator();
-					table = connection.getTable(TableName.valueOf("CVSLOGS"));
+					table = connection.getTable(TableName.valueOf(HBaseConnection.TABLE_NAME));
 					String id = UUID.randomUUID().toString();
 					Put p = new Put(Bytes.toBytes(id));
 					while (it.hasNext()) {
 						String key = it.next();
-						p.addColumn(Bytes.toBytes("RXCONNECT"),
+						p.addColumn(Bytes.toBytes(HBaseConnection.COLUMN_FAMILY),
 								Bytes.toBytes(key),
 								Bytes.toBytes(object.get(key).getAsString()));
 					}
